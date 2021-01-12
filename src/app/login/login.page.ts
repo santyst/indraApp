89b297f784d86e103f0b5f8cd7a58486d14557a4
Alejandro Mejia <alejomejia1@gmail.com>
2021-01-12 09:41:15 -0500

    Cambio credenciales login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Network } from '@ionic-native/network/ngx';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { finalize } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -16,14 +17,28 @@ export class LoginPage implements OnInit {
     password: ''
   };
 
-  constructor(private router: Router, private auth: AuthService, private alertCtrl: AlertController, public network: Network) { }
+  constructor(private router: Router, private auth: AuthService, private alertCtrl: AlertController, public network: Network,
+              private loadingController: LoadingController) { }
 
   ngOnInit() {
    
   }
   
-  login() {
-    this.auth.login(this.credenciales).subscribe(async res => {
+  async login() {
+    const loading = await this.loadingController.create({
+      message: 'Por favor espere...',
+      mode: 'ios',
+      spinner: 'dots'
+    });
+
+    this.auth.login(this.credenciales)
+    .pipe(
+      finalize(() => {
+        loading.dismiss();
+      })
+    )
+    .
+    subscribe(async res => {
       if (res) {
         this.router.navigate(['user-data']);
       } else {
