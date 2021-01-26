7e085840fd65eb61cf02932d7b22bac802c9f3c8
Saul H. Suarez Florez <saulhsf@gmail.com>
2021-01-25 20:59:11 -0500

    style: adaptando imagen del login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Network } from '@ionic-native/network/ngx';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { finalize } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, AfterViewInit {
+  @ViewChild('containerForm', { read: ElementRef }) containerForm: ElementRef;
+  /**
+   * los estilos del logo ecopetrol
+   */
+  styleLogo: {
+    width: number;
+    height: number;
+    marginHorizontal: number;
+  };
 
   credenciales = {
     email: '',
@@ -18,12 +28,38 @@ export class LoginPage implements OnInit {
   };
 
   constructor(private router: Router, private auth: AuthService, private alertCtrl: AlertController, public network: Network,
-              private loadingController: LoadingController) { }
+              private loadingController: LoadingController, private animationCtrl: AnimationController) {
+                this.styleLogo = {
+                  width: window.innerWidth / 2,
+                  height: (window.innerWidth / 2) / 2.5,
+                  marginHorizontal: window.innerWidth / 4
+                };
+              }
 
   ngOnInit() {
-   
   }
-  
+
+  async ngAfterViewInit() {
+    // animations
+    await this.animationStart();
+  }
+
+  async animationStart() {
+    if (this.containerForm) {
+      console.log('entro entro');
+      const loadingAnimation = this.animationCtrl.create('loading-animation')
+        .addElement(this.containerForm.nativeElement)
+        .duration(500)
+        .delay(2500)
+        .iterations(1)
+        .fromTo('transform', 'scale(0.4)', 'scale(1)');
+        // Don't forget to start the animation!
+      loadingAnimation.play();
+    } else {
+      console.log('no entro');
+    }
+  }
+
   async login() {
     const loading = await this.loadingController.create({
       message: 'Por favor espere...',
