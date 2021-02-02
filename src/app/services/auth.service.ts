0ef8463e3e8118d2b8f0b5131020e6922fc71168
Alejandro Mejia <alejomejia1@gmail.com>
2021-02-02 15:45:24 -0500

    Api de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,8 +4,9 @@ import { Storage } from '@ionic/storage';
 import { BehaviorSubject, Observable, from, of } from 'rxjs';
 import { take, map, switchMap } from 'rxjs/operators';
 import { JwtHelperService } from "@auth0/angular-jwt";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 const helper = new JwtHelperService();
 const TOKEN_KEY = 'jwt-token';
@@ -17,6 +18,10 @@ export class AuthService {
 
   public user: Observable<any>;
   private userData = new BehaviorSubject(null);
+  respuesta: any;
+  respuestaF = false;
+  userInfo: any;
+  AuthUrl = `https://bio01.qaingenieros.com/api/sec/auth`;
 
   constructor(private storage: Storage, private http: HttpClient, private plt: Platform, private router: Router) { 
     this.loadStoredToken();
@@ -41,21 +46,39 @@ export class AuthService {
     );
   }
 
-  login(credenciales: {email: string, password: string }) {
-    // Normally make a POST request to your APi with your login credentials
-    if (credenciales.email != 'enrolamiento01@qaingenieros.com' || credenciales.password != 'B103CP21') {
-      return of(null);
-    }
- 
-    return this.http.get('https://jsonplaceholder.typicode.com/posts').pipe(
+  login(credenciales: {client_id: string, client_secret: string }) {
+   let data: Observable<any>;
+    data = this.http.post(`${this.AuthUrl}`, credenciales);
+    data.subscribe((res: any) => {
+      this.respuestaF = res.success;
+      this.respuesta = {
+        token: res.token,
+        expiresin: res.expiresin
+      }
+      this.userInfo = this.respuesta;
+    }, res => {}, () => {
+      if (this.respuestaF === false || credenciales.client_id === '' || credenciales.client_secret === '') {
+        console.log(': ', this.respuestaF);
+        return of(null);
+      }else{
+        this.respuestaF = true; 
+        console.log('this.respuesta: ', this.respuesta);
+        return true;
+      }
+    });
+    
+    return this.http.post(`${this.AuthUrl}`, credenciales).pipe(
       take(1),
-      map(res => {
-        return `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkluZHJhIiwiaWF0IjoxNTE2MjM5MDIyfQ.k04Nd9xY6rNeVi5kWiKehMOT0JwUmssFJssfLxsnJdw`;
+      map((res: any) => {
+        return `${res.token}`;
       }),
       switchMap(token => {
         let decoded = helper.decodeToken(token);
+        /* console.log('decoded: ', decoded); */
         this.userData.next(decoded);
- 
+        if(this.respuestaF === false){
+          return of (null);
+        }
         let storageObs = from(this.storage.set(TOKEN_KEY, token));
         return storageObs;
       })
@@ -64,13 +87,14 @@ export class AuthService {
   }
  
   getUser() {
-    return this.userData.getValue();
+    return this.userInfo;
   }
  
   logout() {
     this.storage.remove(TOKEN_KEY).then(() => {
       this.router.navigateByUrl('/login');
       this.userData.next(null);
+      this.userInfo === '';
     });
   }
 }
